78fe19a86458af5ae63aa951d142ff62ce7b80be
李开 <925701025@qq.com>
2022-03-27 20:07:22 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,18 @@
  * outputDir: 指定打包目录
  * publicPath: 判断环境 切换访问域名
  */
- module.exports = {
-  outputDir: __dirname + '/../server/admin',
-  publicPath: process.env.NODE_ENV === 'production' ? '/admin' : '/',
-  devServer:{
-      proxy: {
-          '/uploads': {
-              target: 'http://server:3000',
-              ws: true,
-              changeOrigin: true
-          }
-      }
-  }
-}
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+module.exports = {
+  publicPath: "./",
+  indexPath: "index.html",
+  outputDir: process.env.outputDir || "dist",
+  assetsDir: "static",
+  lintOnSave: false,
+  runtimeCompiler: true,
+  productionSourceMap: !IS_PROD,
+  parallel: require("os").cpus().length > 1,
+  pwa: {},
+  configureWebpack: {
+    plugins: [],
+  },
+};
